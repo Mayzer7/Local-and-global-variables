f2637240c2ac4b33c410d1f1d09d38cbfdd57f53
Анатолий Корзунов <144378690+Mayzer7@users.noreply.github.com>
2023-10-17 11:59:50 +0300

    Update main.cpp

--- Локальные и глобальные переменные C++/main.cpp
+++ Локальные и глобальные переменные C++/main.cpp
@@ -6,20 +6,20 @@ int a = 100, b = 20;                                     // Глобальные
 
 void f1(int a)                                           // Локальная переменная a
 {
-	a += 10;
-	cout << "f1:\t" << a << "\t" << ::a << endl;
+	a += 10;					 // 1
+	cout << "f1:\t" << a << "\t" << ::a << endl;	 // 2
 }
  
 void f2(int b)                                           // Локальная переменная b
 {
-	b *= 2;
-	cout << "f2:\t" << b << "\t" << a << endl;
+	b *= 2;						 // 3
+	cout << "f2:\t" << b << "\t" << a << endl;	 // 4
 }
 
 int main()
 {
-	cout << "main:\t" << a << "\t" << b << endl;
-	f1(a); f2(b);
-	cout << "main:\t" << a << "\t" << b << endl;
+	cout << "main:\t" << a << "\t" << b << endl;	 // 5
+	f1(a); f2(b);                             	 // 6
+	cout << "main:\t" << a << "\t" << b << endl;	 // 7
 	return 0;
 }
